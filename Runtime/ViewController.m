0405f662df2b1c2cd396132b98f7964ac90acefe
YuSiyuan <yusiyuan05@meituan.com>
2021-07-05 18:00:13 +0800

    message_redirection

--- Runtime/ViewController.m
+++ Runtime/ViewController.m
@@ -6,6 +6,17 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+
+@interface Person : NSObject
+
+@end
+
+@implementation Person
+- (void)myTestPrint:(NSString *)str {
+    NSLog(@"sy%@", str);
+}
+@end
 
 @interface ViewController ()
 
@@ -15,6 +26,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self performSelector:@selector(myTestPrint:) withObject:@", hello!"];
+}
+
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    return YES;
+}
+
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    return nil;
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    if ([NSStringFromSelector(aSelector) isEqualToString:@"myTestPrint:"]) {
+        return [NSMethodSignature signatureWithObjCTypes:"v@:"];
+    }
+    return [super methodSignatureForSelector:aSelector];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    SEL sel = anInvocation.selector;
+    Person *p = [Person new];
+    if ([p respondsToSelector:sel]) {
+        [anInvocation invokeWithTarget:p];
+    } else {
+        [self doesNotRecognizeSelector:sel];
+    }
 }
 
 
