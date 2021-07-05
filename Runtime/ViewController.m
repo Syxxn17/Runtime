d929761f3a69759b18249905fe9a0d8ece0914b7
YuSiyuan <yusiyuan05@meituan.com>
2021-07-05 17:42:13 +0800

    message_accepter_redirection

--- Runtime/ViewController.m
+++ Runtime/ViewController.m
@@ -6,6 +6,18 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+
+@interface Person : NSObject
+- (void)myTestPrint: (NSString *)str;
+@end
+
+@implementation Person
+- (void)myTestPrint:(NSString *)str {
+    NSLog(@"sy%@", str);
+}
+@end
+
 
 @interface ViewController ()
 
@@ -15,7 +27,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self performSelector:@selector(myTestPrint:) withObject:@", hello!"];
 }
 
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    if (sel == @selector(myTestPrint:)) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    if (aSelector == @selector(myTestPrint:)) {
+        return [Person new];
+    } else {
+        return [super forwardingTargetForSelector:aSelector];
+    }
+}
 
 @end
