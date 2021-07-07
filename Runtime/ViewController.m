607e4575ff65024d68ba53fa8422f349217d05f7
YuSiyuan <yusiyuan05@meituan.com>
2021-07-07 14:34:38 +0800

    macro definition method

--- Runtime/ViewController.m
+++ Runtime/ViewController.m
@@ -8,14 +8,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong)UILabel *label;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view addSubview:self.label];
 }
 
+-(UILabel *)label {
+    if (!_label) {
+        _label = [[UILabel alloc] initWithFrame:CGRectMake(50, 50, 100, 50)];
+        _label.text = @"label";
+        _label.backgroundColor = [UIColor blueColor];
+        _label.font = [UIFont fontWithName:@"Helvetica-BoldOblique" size:Fontsize(19)];
+        _label.textColor = [UIColor whiteColor];
+    }
+    return _label;
+}
 
 @end
